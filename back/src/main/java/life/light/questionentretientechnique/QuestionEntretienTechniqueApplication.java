552eade47093e83c205e905f122441aed1d5f984
life light <lifelightdev@gmail.com>
2023-10-16 11:10:12 +0200

    Initialisation du projet

---
+++ back/src/main/java/life/light/questionentretientechnique/QuestionEntretienTechniqueApplication.java
@@ -0,0 +1,13 @@
+package life.light.questionentretientechnique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionEntretienTechniqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionEntretienTechniqueApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/life/light/questionentretientechnique/QuestionEntretienTechniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package life.light.questionentretientechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionEntretienTechniqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
